66ee57b927e1cb3bbb56ab0c275b7e1d0af4c6e4
ViruzDVL <meyzalbagas@gmail.com>
2026-06-16 15:06:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "online",
+        message: "Roblox Presence Proxy Running"
+    });
+});
+
+module.exports = app;
